caca684f9a83c20c55fe719f65c5762850c034f4
robin-hassan <robiul.hassan12102@gmail.com>
2020-08-22 19:21:29 +0800

    added landing page

---
+++ src/app/components/back-icon/back-icon.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'rh-back-icon',
+  templateUrl: './back-icon.component.html',
+  styleUrls: ['./back-icon.component.scss']
+})
+export class BackIconComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
